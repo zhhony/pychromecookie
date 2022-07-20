603040d081b5d572479c95a06e7928e341de62d9
zhhony <zhhony@126.com>
2022-07-20 13:21:04 +0800

    完善conn上下文管理器类

---
+++ modules/__init__.py
@@ -0,0 +1,1 @@
+from ._conn import _conn
--- modules/_conn.py
+++ modules/_conn.py
@@ -2,15 +2,18 @@ import sqlite3
 import os
 from typing import *
 
+from pygame import Cursor
+
 
 class _conn(sqlite3.Connection):
     def __init__(self, path) -> None:
         super().__init__(path)
+        self._path = path
 
-    def __enter__(self):
+    def __enter__(self) -> Cursor:
         return self.cursor()
 
-    def __exit__(self, exc_type, exc_value, exc_traceback):
+    def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
         self.cursor().close()
         self.close()
         if exc_traceback is None:
@@ -18,16 +21,6 @@ class _conn(sqlite3.Connection):
         else:
             ...
 
+    def __repr__(self) -> str:
+        return '_conn(path = %s)' % self._path
 
-CHROME_COOKIE_PATH = r'\Google\Chrome\User Data\Default\network\Cookies'
-CHROME_LOCALSTATE_PATH = r'\Google\Chrome\User Data\Local State'
-
-path = os.environ['LOCALAPPDATA']
-dbCookies = path + CHROME_COOKIE_PATH  # cookie文件，本质是一个sqlite数据库
-fileLocalState = path + CHROME_LOCALSTATE_PATH  # 存储密钥的文件，本质是一个Json文件
-
-
-with _conn(dbCookies) as cur:
-    sql = """select creation_utc,host_key,name,encrypted_value,path from cookies where host_key like '%baidu%'"""
-    cur.execute(sql)
-    valCookies = cur.fetchall()
