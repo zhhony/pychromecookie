4ec0dea944e7084a6295c593e499a6ac7f4d365d
zhhony <zhhony@126.com>
2022-07-22 10:35:41 +0800

    完善爬取代码

--- app.py
+++ app.py
@@ -14,20 +14,25 @@ for i in range(1, 25):
     urlList.append('http://www.stats.gov.cn/tjsj/zxfb/index_%d.html' % i)
 
 
-# re模块资源
+# re模块-----------------------------------------------------------
+# 1、用于匹配标题链接的开头、结尾部分
+URL_FOR_TITLE = '<li>\n\t\t\t\t\t\t<a href="'
+URL_FOR_TITLE_END = r'" target="_blank" >'
 
-HEAD_1 = r'<font class="cont_tit03">'
-HEAD_2 = r'<font class="cont_tit02">'
-END = '</font>'
+# 2、用于匹配标题本身以及对应发布时间的开头、结尾部分
+HEAD_FOR_TITLE = r'<font class="cont_tit03">'
+HEAD_FOR_TIMES = r'<font class="cont_tit02">'
+HEAD_FOR_END = '</font>'
 
-# paraTitle = re.compile(r'(?<=<font class="cont_tit03">)[\d\D\w\W\s\S\u4E00-\u9FA5 .%]*?(?=</font>)' )
-# paraTime = re.compile(r'(?<=<font class="cont_tit02">)[\d\D\w\W\s\S\u4E00-\u9FA5 .%]*?(?=</font>)' )
+# 3、用于匹配任意长度的任意字符或符号
+ANY_TEXT = r'[\d\D\w\W\s\S\u4E00-\u9FA5 .%]*?'
 
-paraTitle = re.compile(f'(?<={HEAD_1})[\d\D\w\W\s\S\u4E00-\u9FA5 .%]*?(?={END})')
-paraTime = re.compile(f'(?<={HEAD_2})[\d\D\w\W\s\S\u4E00-\u9FA5 .%]*?(?={END})')
+# 4、正则表达式
+para = re.compile(f'(?P<url>(?<={URL_FOR_TITLE}){ANY_TEXT}(?={URL_FOR_TITLE_END}))(?P<split_1>{ANY_TEXT})(?P<title>(?<={HEAD_FOR_TITLE}){ANY_TEXT}(?={HEAD_FOR_END}))(?P<split_2>{ANY_TEXT})(?P<time>(?<={HEAD_FOR_TIMES}){ANY_TEXT}(?={HEAD_FOR_END}))')
 
 
-headers={
+# request模块
+headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -43,25 +48,27 @@ headers={
 
 }
 
-cookie_jar=MozillaCookieJar('./log/Cookie1658392515.log')
+cookie_jar = MozillaCookieJar('./log/Cookie1658392515.log')
 cookie_jar.load(ignore_discard=True, ignore_expires=True)
 
-cookie_processor=HTTPCookieProcessor(cookie_jar)
-opener=build_opener(cookie_processor)
+cookie_processor = HTTPCookieProcessor(cookie_jar)
+opener = build_opener(cookie_processor)
 
 
-data=[]
+data = []
 for url in urlList:
-    request=Request(url, headers=headers, method='GET')
+    request = Request(url, headers=headers, method='GET')
 
-    response=opener.open(request)
-    html=gzip.decompress(response.read()).decode('utf8')
-    title=re.findall(paraTitle, html)
-    data.append(title)
+    response = opener.open(request)
+    html = gzip.decompress(response.read()).decode('utf8')
 
-with open(os.environ['USERPROFILE'] + r"\Desktop\Untitled-1.html", 'w') as f:
-    f.write(html)
+    title = re.finditer(para, html)
+    for i in title:
+        data.append(list(i.group('url', 'title', 'time')))
 
 
-cookie_jar.save('./log/Cookie'+str(int(time.time())) + '.log',
-                ignore_discard=True, ignore_expires=True)
+# with open(os.environ['USERPROFILE'] + r"\Desktop\Untitled-1.html", 'w') as f:
+#     f.write(html)
+
+# cookie_jar.save('./log/Cookie'+str(int(time.time())) + '.log',
+#                 ignore_discard=True, ignore_expires=True)
